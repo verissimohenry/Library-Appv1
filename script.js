5d2d5b94f203d5851b68a40226cf3699b3f22815
verissimohenry <verissimohenry04@gmail.com>
2021-07-01 14:28:48 +0100

    modify script page

--- script.js
+++ script.js
@@ -10,3 +10,13 @@ const screen = document.getElementById('screen');
 const form = document.getElementById('form');
 const button = document.getElementById('add-book');
 const submit = document.getElementById('submit-book');
+
+function openForm() {
+    form.classList.toggle('form2');
+    screen.classList.toggle('form2');
+    closeButton.classList.toggle('form2');
+}
+
+button.addEventListener('click', openForm);
+submit.addEventListener('click', openForm);
+closeButton.addEventListener('click', openForm);
