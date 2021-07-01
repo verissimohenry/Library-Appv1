3c5fb8c2baedfe0cc139f2f9c0e99b7ca992a4e4
verissimohenry <verissimohenry04@gmail.com>
2021-07-01 14:27:40 +0100

    modify script js file

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const myLibrary = [];
+const body = document.getElementById('body');
+let title;
+let author;
+let pages;
+let image;
 
+const closeButton = document.getElementById('close-button');
+const screen = document.getElementById('screen');
+const form = document.getElementById('form');
+const button = document.getElementById('add-book');
+const submit = document.getElementById('submit-book');
