d0575b9b094891b32fa1110328fa5aaa55d176a4
verissimohenry <verissimohenry04@gmail.com>
2021-07-01 15:30:46 +0100

    fix linter error

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ function localStorageSetter() {
   for (let i = 0; i < myLibrary.length; i += 1) {
     const book = myLibrary[i];
     if (book !== undefined) {
-        localStorage.setItem(`book${i + 1}`, JSON.stringify(book));
+      localStorage.setItem(`book${i + 1}`, JSON.stringify(book));
     }
   }
 }
@@ -55,11 +55,11 @@ function changeReadButton(btnRead) {
   btnRead.addEventListener('click', (e) => {
     const i = e.target.id.split('').pop();
     if (e.target.innerHTML === 'Read') {
-        myLibrary[i].read = false;
-        e.target.innerHTML = 'Not Read';
+      myLibrary[i].read = false;
+      e.target.innerHTML = 'Not Read';
     } else {
-        myLibrary[i].read = true;
-        e.target.innerHTML = 'Read';
+      myLibrary[i].read = true;
+      e.target.innerHTML = 'Read';
     }
     localStorageSetter();
   });
