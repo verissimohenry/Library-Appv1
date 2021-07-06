03d09ca16db624d27459f33f1a888af338ed58a0
verissimohenry <verissimohenry04@gmail.com>
2021-07-06 17:41:27 +0100

    fix linter error

--- script.js
+++ script.js
@@ -32,10 +32,9 @@ const Book = (
   title,
   author,
   pages,
-  image = 'https://st4.depositphotos.com/14953852/24787/v/600/depositphotos_247872612-stock-illustration-no-image-available-icon-vector.jpg',
-  read = false,
-) => 
-{
+  image = "https://st4.depositphotos.com/14953852/24787/v/600/depositphotos_247872612-stock-illustration-no-image-available-icon-vector.jpg",
+  read = false
+) => {
   return { title, author, pages, image, read };
 };
 
